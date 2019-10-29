50ec7434260cb227d7ec3cfcb5d389bc17186899
mudrakpatel <mudrakpatel96@gmail.com>
2019-10-29 16:07:57 +0530

    getScreams function created and deployed to firebase. The function works

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,23 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+//Initialize application
+admin.initializeApp();
+
+//getScreams function
+exports.getScreams = functions.https.onRequest((request, response) => {
+    admin
+    .firestore()
+    .collection('screams')
+    .get()
+    .then(data => {
+        let screams = [];
+        data.forEach(document => {
+            screams.push(document.data());
+        });
+        return response.status(200).json(screams);
+    })
+    .catch(err => {
+        return response.status(500).json({error: err.message});
+    });
 });
