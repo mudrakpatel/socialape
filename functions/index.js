294ae551f4d53c0b2de8b218a0a4d3dc611b9301
mudrakpatel <mudrakpatel96@gmail.com>
2019-10-29 16:51:49 +0530

    addScream function created and deployed to firebase. The function works

--- functions/index.js
+++ functions/index.js
@@ -21,3 +21,27 @@ exports.getScreams = functions.https.onRequest((request, response) => {
         return response.status(500).json({error: err.message});
     });
 });
+
+//addScream function
+exports.addScream = functions.https.onRequest((request, response) => {
+    const newScream = {
+        body: request.body.body,
+        userHandle: request.body.userHandle,
+        createdAt: admin.firestore.Timestamp.fromDate(new Date()),
+    };
+
+    admin
+    .firestore()
+    .collection('screams')
+    .add(newScream)
+    .then(document => {
+        return response.status(201).json({
+            message: `Document: ${document.id} created successfully`,
+        });
+    })
+    .catch(err => {
+        return response.status(500).json({
+            error: err.message,
+        });
+    });
+});
