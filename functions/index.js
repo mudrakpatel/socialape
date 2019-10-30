fb303c67d1d964cb4e49bb4eecd344e134e04fa9
mudrakpatel <mudrakpatel96@gmail.com>
2019-10-30 18:23:20 +0530

    Token is not returned by signup api

--- functions/index.js
+++ functions/index.js
@@ -1,62 +1,114 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-const express = require('express');
+const firebase = require('firebase');
+//Require express and call the 
+//express function in the sameline
+const app = require('express')();
 
-//Initialize Firebase application
-admin.initializeApp();
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyDlr92_c-e59rhX_hbPLEOIE3W0NaSsImg',
+  authDomain: 'socialapemudrak.firebaseapp.com',
+  databaseURL: 'https://socialapemudrak.firebaseio.com',
+  projectId: 'socialapemudrak',
+  storageBucket: 'socialapemudrak.appspot.com',
+  messagingSenderId: '262707378891',
+  appId: '1:262707378891:web:b44608d4ad5e0ba2dab94e',
+  measurementId: 'G-YP2ZGHSQHS'
+};
 
-//Initialize express app
-const app = express();
+// Initialize Firebase application
+firebase.initializeApp(firebaseConfig);
+admin.initializeApp(firebaseConfig);
+//Initialize firestore database
+const db = admin.firestore();
 
-//Let Firebase know that app is 
-//the container for all our routes
+// Let Firebase know that app is 
+// the container for all our routes
 exports.api = functions.https.onRequest(app);
 
-//Get all Screams
+// Get all Screams
 app.get('/screams', (request, response, next) => {
-    admin
-    .firestore()
+  db
     .collection('screams')
     .orderBy('createdAt', 'desc')
     .get()
     .then(data => {
-        let screams = [];
-        data.forEach(document => {
-            screams.push({
-                screamId: document.id,
-                body: document.data().body,
-                userHandle: document.data().userHandle,
-                createdAt: document.data().createdAt,
-            });
+      let screams = []
+      data.forEach(document => {
+        screams.push({
+          screamId: document.id,
+          body: document.data().body,
+          userHandle: document.data().userHandle,
+          createdAt: document.data().createdAt
         });
-        return response.status(200).json(screams);
+      });
+      return response.status(200).json(screams)
     })
     .catch(err => {
-        return response.status(500).json({error: err.message});
+      return response.status(500).json({error: err.message});
     });
 });
 
-//Add a Scream
+// Add a Scream
 app.post('/scream', (request, response) => {
-    const newScream = {
-        body: request.body.body,
-        userHandle: request.body.userHandle,
-        createdAt: new Date().toISOString(),
-    };
-    
-    //Add the Scream object to firestore database
-    admin
-    .firestore()
+  const newScream = {
+    body: request.body.body,
+    userHandle: request.body.userHandle,
+    createdAt: new Date().toISOString()
+  };
+
+  // Add the Scream object to firestore database
+  db
     .collection('screams')
     .add(newScream)
     .then(document => {
-        return response.status(201).json({
-            message: `Document: ${document.id} created successfully`,
-        });
+      return response.status(201).json({
+        message: `Document: ${document.id} created successfully`
+      });
     })
     .catch(err => {
-        return response.status(500).json({
-            error: err.message,
-        });
+      return response.status(500).json({
+        error: err.message
+      });
     });
+});
+
+//Sign Up route
+app.post('/register', (request, response) => {
+  const newUser = {
+    email: request.body.email,
+    password: request.body.password,
+    confirmPassword: request.body.confirmPassword,
+    handle: request.body.handle,
+  };
+  //TODO: Validate data
+  db
+  .doc(`/users/${newUser.handle}`)
+  .get()
+  .then((document) => {
+    if(document.exists){
+      return response.status(400).json({
+        handle: `This Handle is already taken`
+      });
+    } else {
+      return firebase.auth()
+        .createUserWithEmailAndPassword(
+        newUser.email, newUser.password);
+    }
+  })
+  .then((data) => {
+    if(!data.user){
+      return response.status(500).json({message: 'No data.user'});
+    }else{
+      return response.status(201).json({data});
+    }
+    //return data.user.getIdToken(); //Token is not being returned
+  })
+  // .then((token) => {
+  //   return response.status(201).json({token: token});
+  // })
+  .catch((err) => {
+    return response.status(500).json({error: err.message});
+  });
 });
