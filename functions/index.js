cb81548ab31b12c08f9dd807bff3c7fd154e1ac0
mudrakpatel <mudrakpatel96@gmail.com>
2019-11-22 16:28:18 -0500

    db trigger functions for Notifications written

--- functions/index.js
+++ functions/index.js
@@ -3,6 +3,7 @@ const functions = require('firebase-functions');
 //express function in the sameline
 const app = require('express')();
 
+const {db} = require('./util/admin');
 const {firebaseAuthMiddleware} = require('./middlewares/auth');
 const {
     getAllScreams,
@@ -35,6 +36,73 @@ app.post('/login', login);
 app.post('/user/image', firebaseAuthMiddleware, uploadImage);
 app.post('/user', firebaseAuthMiddleware, addUserDetails);
 app.get('/user', firebaseAuthMiddleware, getAuthenticatedUser);
+
+//Notifications trigger functions
+//Create a notification document when a Scream is liked
+exports.createNotificationOnLike = functions.firestore.document('likes/${id}')
+    .onCreate((snapshot) => {
+        db.doc(`/screams/${snapshot.data().screamId}`).get()
+          .then((document) => {
+              //If the Scream document exists, create a notification
+              if(document.exists){
+                //Give the Notification document the
+                //same id as the like document.
+                return db.doc(`/notifications/${snapshot.id}`).set({
+                    createdAt: new Date().toISOString(),
+                    recipient: document.data().userHandle,
+                    sender: snapshot.data().userHandle,
+                    type: 'like',
+                    read: false,
+                    screamId: document.id,
+                });
+              }
+          }).then(() => {
+              return;
+          }).catch((err) => {
+              console.log(err);
+              //No need to send a response because this a
+              //database trigger and not an API endpoint.
+              return;
+          });
+    });
+
+//Delete the notification for the Scream liked when
+//it is unliked by the same user who liked the Scream.
+exports.deleteNotificationOnUnLike = functions.firestore.document('likes/${id}')
+    .onDelete((snapshot) => {
+        db.doc(`/notifications/${snapshot.id}`).delete().then(() => {
+            return;
+        }).catch((err) => {
+            console.log(err);
+            return;
+        });
+    });
+
+//Create a Notification when someone comments on a Scream
+exports.createNotificationOnComment = functions.firestore.document('comments/${id}')
+    .onCreate((snapshot) => {
+        db.doc(`/screams/${snapshot.data().screamId}`).get()
+            .then((document) => {
+                //If the Scream document exists, create a notification
+                if (document.exists) {
+                    //Give the Notification document the
+                    //same id as the comment document.
+                    return db.doc(`/notifications/${snapshot.id}`).set({
+                        createdAt: new Date().toISOString(),
+                        recipient: document.data().userHandle,
+                        sender: snapshot.data().userHandle,
+                        type: 'comment',
+                        read: false,
+                        screamId: document.id,
+                    });
+                }
+            }).then(() => {
+                return;
+            }).catch((err) => {
+                console.log(err);
+                return;
+            });
+    });
 
 // Let Firebase know that app is 
 // the container for all our routes
