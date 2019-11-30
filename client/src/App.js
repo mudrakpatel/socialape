0f6868ebb3013621c7d53d9a11b90a4544b221b5
mudrakpatel <mudrakpatel96@gmail.com>
2019-11-30 16:44:58 -0500

    Working on Navbar

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
 
 import './App.css';
+import Navbar from './components/Navbar';
 import Home from './pages/home';
 import Login from './pages/login';
 import Signup from './pages/signup';
@@ -10,11 +11,14 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Switch>
-          <Route exact path="/" component={Home}/>
-          <Route path="/signup" component={Signup}/>
-          <Route path="/login" component={Login}/>
-        </Switch>
+        <Navbar/>
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={Home}/>
+            <Route path="/signup" component={Signup}/>
+            <Route path="/login" component={Login}/>
+          </Switch>
+        </div>
       </BrowserRouter>
     </div>
   );
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,12 +1,18 @@
 import React, {Component} from 'react';
+import Link from 'react-router-dom/Link';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
 
 class Navbar extends Component{
     render(){
         return(
-            <AppBar /*default value for 'position' attribute is 'fixed'*/>
-                
+            < AppBar > {/*default value for 'position' attribute is 'fixed'*/}
+                <Toolbar>
+                    <Button color='inherit'>Home</Button>
+                    <Button color='inherit'>Login</Button>
+                    <Button color='inherit'>Signup</Button>
+                </Toolbar>
             </AppBar>
         )
     }
