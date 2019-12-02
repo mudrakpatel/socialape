2d5a5ff6bc31a76d30ee9e437f88d47f83edd84a
mudrakpatel <mudrakpatel96@gmail.com>
2019-12-01 20:50:14 -0500

    Scream Card Details done

--- client/src/components/Scream.js
+++ client/src/components/Scream.js
@@ -3,15 +3,24 @@ import withStyles from '@material-ui/core/styles/withStyles';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+import Link from 'react-router-dom/Link';
 
 const styles = {
-    //We will have a card
     card: {
         display: 'flex',
-    }
+        marginBottom: 20,
+    },
+    image: {
+        minWidth: 200,
+    },
+    content: {
+        padding: 25,
+        objectFit: 'cover',
+    },
 };
 
-const Scream = () => {
+const Scream = (props) => {
     const {classes, scream: {
         body, 
         createdAt, 
@@ -20,12 +29,19 @@ const Scream = () => {
         screamId,
         likeCount,
         commentCount,
-    }} = this.props;
+    }} = props;
     return(
-        <Card>
-            <CardMedia image={userImage} title="Profile image"/>
-            <CardContent>
-                
+        <Card className={classes.card}>
+            <CardMedia image={userImage} title="Profile image" className={classes.image}/>
+            <CardContent className={classes.content}>
+                <Typography variant="h5" component={Link}
+                    to={`/users/${userHandle}`} color="primary">
+                    {userHandle}
+                </Typography>
+                <Typography variant="body2" color="textSecondary">
+                    {createdAt}
+                </Typography>
+                <Typography variant="body1">{body}</Typography>
             </CardContent>
         </Card>
     );
