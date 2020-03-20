d8b15ca9b54ac9ab1246c4d9516f805a32e2952f
mudrakpatel <mudrakpatel96@gmail.com>
2020-03-20 13:16:12 -0400

    Working on uiReducer

---
+++ client/src/redux/reducers/uiReducer.js
@@ -0,0 +1,33 @@
+//Import error action types
+import {SET_ERRORS, CLEAR_ERRORS, LOADING_UI} from '../types';
+
+//Initial state for uiReducer
+const initialState = {
+    loading: false,
+    errors: null,
+};
+
+//Handle the dispatched actions
+export default (state = initialState, action) => {
+    switch(action.type){
+        case SET_ERRORS:
+            return {
+                ...state,
+                loading: false,
+                errors: action.payload,
+            };
+        case CLEAR_ERRORS:
+            return {
+                ...state,
+                loading: false,
+                errors: null,
+            };
+        case LOADING_UI:
+            return {
+                ...state,
+                loading: true,
+            };
+        default:
+            return state;
+    }
+};
